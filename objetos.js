52a1248ccaee9a3818dffe507b1dcb47f59e5819
Rubén <rmartinezdominguez@danielcastelao.org>
2022-01-19 17:42:11 +0100

    Añadido objetos.js

---
+++ objetos.js
@@ -0,0 +1,5 @@
+var Vehiculo = function(nombre_vehiculo, tipo_vehiculo, id_vehiculo) {
+    Vehiculo.prototype.nombre_vehiculo = nombre_vehiculo
+    Vehiculo.prototype.tipo_vehiculo = tipo_vehiculo
+    Vehiculo.prototype.id_vehiculo = id_vehiculo
+}
